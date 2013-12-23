889f11c72fb7fb4536f0f5445281e1f316a12baf
bleujin <bleujin@gmail.com>
2013-12-23 12:23:57 +0900

    first

---
+++ talkserver/src/net/ion/talk/let/ScriptTalkHandler.java
@@ -0,0 +1,46 @@
+package net.ion.talk.let;
+
+import net.ion.craken.aradon.bean.RhinoEntry;
+import net.ion.craken.node.ReadSession;
+import net.ion.script.rhino.ResponseHandler;
+import net.ion.script.rhino.RhinoScript;
+import net.ion.talk.ParameterMap;
+import net.ion.talk.TalkEngine;
+import net.ion.talk.TalkHandler;
+import net.ion.talk.TalkMessage;
+import net.ion.talk.UserConnection;
+
+public class ScriptTalkHandler implements TalkHandler {
+
+	@Override
+	public void onClose(TalkEngine tengine, UserConnection uconn) {
+		
+	}
+
+	@Override
+	public void onConnected(TalkEngine tengine, UserConnection uconn) {
+		
+	}
+
+	@Override
+	public void onEngineStart(TalkEngine tengine) {
+		
+	}
+
+	@Override
+	public void onEngineStop(TalkEngine tengine) {
+		
+	}
+
+	@Override
+	public void onMessage(TalkEngine tengine, UserConnection sender, ReadSession rsession, TalkMessage tmessage) {
+		
+		RhinoEntry rengine = tengine.context().getAttributeObject(RhinoEntry.EntryName, RhinoEntry.class);
+		RhinoScript rscript = rengine.newScript(tmessage.id()).defineScript(tmessage.script());
+		
+		rscript.bind("session", rsession).bind("params", ParameterMap.create(tmessage.params())) ;
+		String scriptResult = rscript.exec(ResponseHandler.StringMessage) ;
+		sender.sendMessage(scriptResult) ;
+	}
+
+}
