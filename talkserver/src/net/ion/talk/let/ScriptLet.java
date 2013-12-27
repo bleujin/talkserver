c12f2205418378808c029b059a0872c7a09bbf37
Ryunhee Han <ryuneeee@gmail.com>
2013-12-27 13:07:09 +0900

    Add: Post function in ScriptLet

--- talkserver/src/net/ion/talk/let/ScriptLet.java
+++ talkserver/src/net/ion/talk/let/ScriptLet.java
@@ -3,17 +3,22 @@ package net.ion.talk.let;
 import net.ion.craken.aradon.bean.RepositoryEntry;
 import net.ion.craken.node.ReadNode;
 import net.ion.craken.node.ReadSession;
+import net.ion.craken.node.TransactionJob;
+import net.ion.craken.node.WriteSession;
 import net.ion.craken.node.crud.ReadChildren;
 import net.ion.framework.util.Debug;
 import net.ion.nradon.let.IServiceLet;
 import net.ion.radon.core.TreeContext;
 import net.ion.radon.core.annotation.AnContext;
 import net.ion.radon.core.annotation.AnRequest;
+import net.ion.radon.core.annotation.AnResponse;
 import net.ion.radon.core.let.InnerRequest;
+import org.restlet.Response;
 import org.restlet.data.Language;
 import org.restlet.data.MediaType;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
 
 import java.io.IOException;
 
@@ -28,14 +33,14 @@ public class ScriptLet implements IServiceLet {
         RepositoryEntry rentry = context.getAttributeObject(RepositoryEntry.EntryName, RepositoryEntry.class);
         ReadSession session = rentry.login("test");
         StringBuilder sb = new StringBuilder();
-        String requestPath = "/script/" + request.getPathReference().getPath();
+        String requestPath = "/script" + request.getPathReference().getPath();
 
-        sb.append("<!DOCTYPE HTML><html><head></head><body><form target=\""+requestPath+"\" method=\"POST\"><textarea style='height:400px; width: 600px;' id=\"script\">");
+        sb.append("<!DOCTYPE HTML><html><head></head><body><form method=\"POST\"><textarea style='height:400px; width: 600px;' name=\"script\">");
         if(session.exists(requestPath)){
             ReadNode node = session.pathBy(requestPath);
             sb.append(node.property("script").stringValue());
             sb.append("</textarea>");
-            sb.append("<br/ ><input type=\"submit\" value=\"submit\">");
+            sb.append("<br/ ><input type=\"submit\" value=\"save\">");
             ReadChildren children = node.children();
             sb.append("<ul>");
             while(children.hasNext()){
@@ -52,4 +57,34 @@ public class ScriptLet implements IServiceLet {
         sb.append("</body></html>");
         return new StringRepresentation(sb.toString(), MediaType.TEXT_HTML, Language.valueOf("UTF-8"));
     }
+
+
+    @Post
+    public StringRepresentation updatePage(@AnContext TreeContext context, @AnRequest InnerRequest request, @AnResponse Response response) throws Exception {
+
+        final String requestPath = "/script" + request.getPathReference().getPath();
+        RepositoryEntry rentry = context.getAttributeObject(RepositoryEntry.EntryName, RepositoryEntry.class);
+        ReadSession session = rentry.login("test");
+
+        final String script = request.getFormParameter().getFirstValue("script").toString();
+
+        session.tranSync(new TransactionJob<Object>() {
+            @Override
+            public Object handle(WriteSession wsession) throws Exception {
+                wsession.createBy(requestPath).property("script", script);
+                return null;
+            }
+        });
+
+        response.redirectPermanent(requestPath);
+        return null;
+    }
 }
+
+
+
+
+
+
+
+
--- talkserver/test/net/ion/talk/let/TestScriptLet.java
+++ talkserver/test/net/ion/talk/let/TestScriptLet.java
@@ -5,7 +5,9 @@ import net.ion.craken.aradon.bean.RepositoryEntry;
 import net.ion.craken.node.ReadSession;
 import net.ion.craken.node.TransactionJob;
 import net.ion.craken.node.WriteSession;
+import net.ion.framework.util.Debug;
 import net.ion.framework.util.InfinityThread;
+import net.ion.framework.util.MapUtil;
 import net.ion.nradon.Radon;
 import net.ion.radon.client.AradonClient;
 import net.ion.radon.client.AradonClientFactory;
@@ -34,37 +36,42 @@ public class TestScriptLet extends TestCase{
         Configuration config = Configuration.newBuilder().aradon()
                 .addAttribute(RepositoryEntry.EntryName, rentry)
                 .sections().restSection("script")
-                .path("/toontalk").addUrlPattern("/{path}").matchMode(EnumClass.IMatchMode.STARTWITH).handler(ScriptLet.class).build();
+                .path("").addUrlPattern("/{path}").matchMode(EnumClass.IMatchMode.STARTWITH).handler(ScriptLet.class).build();
 
 
         ReadSession session = rentry.login("test");
         session.tranSync(new TransactionJob<Void>() {
             @Override
             public Void handle(WriteSession wsession) throws Exception {
-                wsession.createBy("script/test");
-                wsession.createBy("script/test/child1");
-                wsession.createBy("script/test/child2");
+                wsession.createBy("script/test").property("script", "hello");
+                wsession.createBy("script/test/child1").property("script", "ryun");
+                wsession.createBy("script/test/child2").property("script", "bleujin");
                 return null;
             }
         });
 
         aradon = Aradon.create(config);
-        aradon.startServer(9000);
+//        aradon.startServer(9000);
+        ac = AradonClientFactory.create(aradon);
         Radon radon = aradon.toRadon(9000);
         radon.start();
         new InfinityThread().startNJoin();
     }
 
-    public void testGetBasicPage() throws Exception {
-        Response response = ac.createRequest("/script/toontalk/test").handle(Method.GET);
+    public void testGetPage() throws Exception {
+        Response response = ac.createRequest("/script/test").handle(Method.GET);
         assertEquals(200, response.getStatus().getCode());
+        assertTrue(response.getEntityAsText().contains("hello"));
+        assertTrue(response.getEntityAsText().contains("child1"));
+        assertTrue(response.getEntityAsText().contains("child2"));
     }
 
-    public void testGetNode(){
-        Response response = ac.createRequest("/script/toontalk/testScript").handle(Method.GET);
-        assertEquals(200, response.getStatus().getCode());
+    public void testPostPage(){
+        Response response = ac.createRequest("/script/test/post").addParameter("script","HelloTest").handle(Method.POST);
+        assertEquals(301, response.getStatus().getCode());
+        response = ac.createRequest("/script/test/post").handle(Method.GET);
+        assertTrue(response.getEntityAsText().contains("HelloTest"));
     }
-
 
     @Override
     public void tearDown() throws Exception {
