493ccbd2f6ec5ec58048cfb3e57c6701f7e9d7e3
bleujin@gmail.com <bleujin@gmail.com>
2014-02-26 15:30:30 +0900

    mail

---
+++ src/net/ion/framework/mail/Mailer.java
@@ -0,0 +1,100 @@
+package net.ion.framework.mail;
+
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+import javax.mail.FetchProfile;
+import javax.mail.Flags;
+import javax.mail.Flags.Flag;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Store;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.search.FlagTerm;
+
+import org.infinispan.util.concurrent.WithinThreadExecutor;
+
+public class Mailer {
+
+	private MailConfig mconfig;
+	private ExecutorService eservice;
+
+	Mailer(MailConfig mconfig) {
+		this.mconfig = mconfig;
+		this.eservice = new WithinThreadExecutor() ; 
+	}
+	
+	
+	public Mailer executors(ExecutorService eservice){
+		this.eservice = eservice ;
+		return this ;
+	}
+	
+	public Future<Void> unreadMessage(final MessageHandler<Void> messageHandler) {
+		return eservice.submit(new Callable<Void>() {
+			@Override
+			public Void call() throws Exception {
+				Session session = Session.getDefaultInstance(mconfig.prop(), null);
+
+				Store store = null ;
+				Folder inbox = null ;
+				try {
+					store = session.getStore(mconfig.recPortocol().stringValue());
+					store.connect(mconfig.recServerHost(), mconfig.recMailUserId(), mconfig.recMailUserPwd());
+
+					/* Mention the folder name which you want to read. */
+					inbox = store.getFolder("Inbox");
+
+					/* Open the inbox using store. */
+					inbox.open(Folder.READ_ONLY);
+
+					/* Get the messages which is unread in the Inbox */
+					Message messages[] = inbox.search(new FlagTerm(new Flags(Flag.SEEN), false));
+
+					/* Use a suitable FetchProfile */
+					FetchProfile fp = new FetchProfile();
+					fp.add(FetchProfile.Item.ENVELOPE);
+					fp.add(FetchProfile.Item.CONTENT_INFO);
+					inbox.fetch(messages, fp);
+
+					messageHandler.handle(messages);
+				} finally {
+					if (inbox != null) try {inbox.close(true) ;} catch(MessagingException ignroe){};
+					if (store != null) try {store.close();} catch(MessagingException ignroe){};
+				}
+				return null;
+			}
+		}) ;
+	}
+
+	public Future<Void> sendMail(final MessageCreater creater) {
+		return eservice.submit(new Callable<Void>(){
+			@Override
+			public Void call() throws Exception {
+				Properties props = System.getProperties();
+				props.setProperty("mail.smtp.host", mconfig.sendServerHost());
+				props.setProperty("mail.user", mconfig.sendMailUserId()) ;
+				props.setProperty("mail.password", mconfig.sendMailUserPwd()) ;
+
+				Session session = Session.getDefaultInstance(props);
+
+				MimeMessage initMessage = new MimeMessage(session);
+				// Set From: header field of the header.
+				initMessage.setFrom(new InternetAddress(mconfig.sendMailUserId())); // Sender's email ID needs to be mentioned
+				
+				MimeMessage makedMessage = creater.makeMessage(initMessage) ;
+				Transport.send(makedMessage);
+				return null;
+			}
+			
+		}) ;
+	}
+
+}
