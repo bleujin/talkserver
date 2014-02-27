af287c38c10f87a176c9aee65bea2658d0f6696a
Ryunhee Han <ryun@i-on.net>
2014-02-27 17:46:45 +0900

    echobot 명령어 추가

--- src/net/ion/talk/bot/EchoBot.java
+++ src/net/ion/talk/bot/EchoBot.java
@@ -4,10 +4,13 @@ import net.ion.craken.node.ReadSession;
 import net.ion.craken.node.TransactionJob;
 import net.ion.craken.node.WriteNode;
 import net.ion.craken.node.WriteSession;
+import net.ion.framework.util.Debug;
 import net.ion.framework.util.ObjectId;
+import net.ion.framework.util.StringUtil;
 import net.ion.talk.bean.Const;
 
 import java.util.Set;
+import java.util.concurrent.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,6 +24,7 @@ public class EchoBot implements EmbedBot {
     private final ReadSession rsession;
     private String id = "echoBot";
     private String requestURL = "http://localhost:9000/bot";
+    private ScheduledExecutorService es = Executors.newScheduledThreadPool(5);
 
     public EchoBot(ReadSession rsession) {
         this.rsession = rsession;
@@ -41,45 +45,79 @@ public class EchoBot implements EmbedBot {
 
         //if bot
         if (id().equals(sender))
-            sendMessage(roomId, "Hello I'm EchoBot");
+            sendMessage(roomId, "Hello I'm EchoBot", sender);
         else
-            sendMessage(roomId, "Hello! " + sender);
+            sendMessage(roomId, "Hello! " + sender, sender);
     }
 
     @Override
     public void onExit(String roomId, String sender) throws Exception {
 
         if (id().equals(sender))
-            sendMessage(roomId, "Bye~ see you later!");
+            sendMessage(roomId, "Bye~ see you later!", sender);
         else
-            sendMessage(roomId, "Bye! " + sender);
+            sendMessage(roomId, "Bye! " + sender, sender);
     }
 
     @Override
     public void onMessage(String roomId, String sender, String message) throws Exception {
-        sendMessage(roomId, message);
+
+
+        if(StringUtil.startsWith(message, "/help ")){
+            sendMessage(roomId, "안녕하세요! EchoBot입니다. 봇에게 지연된 응답을 받고 싶으면 \"/delay 초\"라고 대답해주세요. 5초후 예: /delay 5", sender);
+        }else if(StringUtil.startsWith(message, "/delay ")){
+            int delay = Integer.parseInt(StringUtil.stripStart(message, "/delay "));
+            sendMessage(roomId, sender + " 사용자에게는 봇이 " +delay +"초 후에 반응합니다.", sender);
+            setDelay(roomId, sender, delay);
+        }else{
+            sendMessage(roomId, message, sender);
+        }
+
     }
 
-    private void sendMessage(final String roomId, final String message) throws Exception {
-        final Set<String> memberList = rsession.pathBy("/rooms/" + roomId + "/members").childrenNames();
+    private void setDelay(final String roomId, final String sender, final int delay) throws Exception {
+
         rsession.tranSync(new TransactionJob<Object>() {
             @Override
             public Object handle(WriteSession wsession) throws Exception {
-                WriteNode messageNode = wsession.pathBy("/rooms/" + roomId + "/messages/" + new ObjectId().toString())
-                        .property(Const.Message.Message, message)
-                        .property(Const.Message.Sender, id())
-                        .property(Const.Room.RoomId, roomId)
-                        .property(Const.Message.Event, Const.Event.onMessage)
-                        .property(Const.Message.ClientScript, "client.room().message(args.message)")
-                        .property(Const.Message.RequestId, new ObjectId().toString());
-
-                for (String member : memberList) {
-                    if (member != id())
-                        messageNode.append(Const.Message.Receivers, member);
-                }
+                wsession.pathBy("/rooms/" + roomId + "/members/" + sender + "/bot/" + id()).property("delay", delay);
                 return null;
             }
         });
+    }
+
+    private void sendMessage(final String roomId, final String message, String sender) throws Exception {
+
+        int delay = rsession.ghostBy("/rooms/" + roomId + "/members/" + sender + "/bot/" + id()).property("delay").intValue(0);
+
+        es.schedule(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                final Set<String> memberList = rsession.pathBy("/rooms/" + roomId + "/members").childrenNames();
+                rsession.tranSync(new TransactionJob<Object>() {
+                    @Override
+                    public Object handle(WriteSession wsession) throws Exception {
+                        WriteNode messageNode = wsession.pathBy("/rooms/" + roomId + "/messages/" + new ObjectId().toString())
+                                .property(Const.Message.Message, message)
+                                .property(Const.Message.Sender, id())
+                                .property(Const.Room.RoomId, roomId)
+                                .property(Const.Message.Event, Const.Event.onMessage)
+                                .property(Const.Message.ClientScript, "client.room().message(args.message)")
+                                .property(Const.Message.RequestId, new ObjectId().toString());
+
+                        for (String member : memberList) {
+                            if (!member.equals(id()))
+                                messageNode.append(Const.Message.Receivers, member);
+                        }
+                        return null;
+                    }
+                });
+                return null;
+            }
+        }, delay, TimeUnit.SECONDS);
+
+
+
     }
 
 
--- test/net/ion/talk/bot/TestEchoBot.java
+++ test/net/ion/talk/bot/TestEchoBot.java
@@ -7,7 +7,9 @@ import net.ion.craken.node.ReadNode;
 import net.ion.craken.node.ReadSession;
 import net.ion.craken.node.TransactionJob;
 import net.ion.craken.node.WriteSession;
+import net.ion.framework.util.Debug;
 import net.ion.framework.util.ObjectId;
+import net.ion.framework.util.StringUtil;
 import net.ion.radon.aclient.NewClient;
 import net.ion.radon.core.Aradon;
 import net.ion.radon.util.AradonTester;
@@ -32,6 +34,7 @@ public class TestEchoBot extends TestCase{
     private ReadSession rsession;
     private RhinoEntry rengine;
     private EchoBot echoBot;
+    private Aradon aradon;
 
     @Override
     public void setUp() throws Exception {
@@ -52,6 +55,15 @@ public class TestEchoBot extends TestCase{
                 return null;
             }
         });
+
+
+        BotManager botManager = BotManager.create(rsession);
+        botManager.registerBot(new EchoBot(rsession));
+
+        aradon = AradonTester.create().register("", "/bot",  EmbedBotLet.class).getAradon().startServer(9000);
+        aradon.getServiceContext().putAttribute(RepositoryEntry.EntryName, rentry);
+        aradon.getServiceContext().putAttribute(RhinoEntry.EntryName, rengine);
+        aradon.getServiceContext().putAttribute(BotManager.class.getCanonicalName(), botManager);
     }
 
 
@@ -77,15 +89,6 @@ public class TestEchoBot extends TestCase{
 
     public void testEchoBot() throws Exception {
 
-        BotManager botManager = BotManager.create(rsession);
-        botManager.registerBot(new EchoBot(rsession));
-
-        Aradon aradon = AradonTester.create().register("", "/bot",  EmbedBotLet.class).getAradon().startServer(9000);
-        aradon.getServiceContext().putAttribute(RepositoryEntry.EntryName, rentry);
-        aradon.getServiceContext().putAttribute(RhinoEntry.EntryName, rengine);
-        aradon.getServiceContext().putAttribute(BotManager.class.getCanonicalName(), botManager);
-
-
         final String notifyId = new ObjectId().toString();
 
         rsession.tranSync(new TransactionJob<Object>() {
@@ -96,7 +99,6 @@ public class TestEchoBot extends TestCase{
 
                 wsession.pathBy("/rooms/1234/messages/testMessage")
                         .property(Const.Message.Message, "Hello World!")
-                        .property(Const.Room.RoomId, "1234")
                         .property(Const.Message.Sender, "ryun")
                         .property(Const.Message.Event, Const.Event.onMessage);
                 wsession.pathBy("/notifies/echoBot/" + notifyId).refTo("message", "/rooms/1234/messages/testMessage");
@@ -112,7 +114,7 @@ public class TestEchoBot extends TestCase{
         bot.onMessage(fakeResponse);
 
 
-        Thread.sleep(100);
+        Thread.sleep(1000);
         Iterator<String> iter = rsession.pathBy("/rooms/1234/messages/").childrenNames().iterator();
 
         String echoMessage = null;
@@ -126,16 +128,85 @@ public class TestEchoBot extends TestCase{
 
         assertEquals("Hello World!", responseMessage.property(Const.Message.Message).stringValue());
         assertEquals("echoBot", responseMessage.property(Const.Message.Sender).stringValue());
-        aradon.getServiceContext().removeAttribute(RepositoryEntry.EntryName);
-        aradon.getServiceContext().removeAttribute(RhinoEntry.EntryName);
-        aradon.stop();
+    }
+
+
+    public void testDelay() throws Exception {
+
+        final String notifyId = new ObjectId().toString();
+
+        rsession.tranSync(new TransactionJob<Object>() {
+            @Override
+            public Object handle(WriteSession wsession) throws Exception {
+                wsession.pathBy("/rooms/1234/members/echoBot");
+                wsession.pathBy("/rooms/1234/members/ryun");
+
+                wsession.pathBy("/rooms/1234/messages/testMessage")
+                        .property(Const.Message.Message, "/delay 2")
+                        .property(Const.Message.Sender, "ryun")
+                        .property(Const.Message.Event, Const.Event.onMessage);
+                wsession.pathBy("/notifies/echoBot/" + notifyId).refTo("message", "/rooms/1234/messages/testMessage");
+                return null;
+            }
+        });
+        TalkResponse fakeResponse = TalkResponseBuilder.create().newInner().property("notifyId", notifyId).build();
+
+        Bot bot = new Bot("echoBot", rsession, NewClient.create());
+        bot.onMessage(fakeResponse);
+
+        Thread.sleep(1000);
+        ReadNode message = readMessage();
+        assertEquals("ryun 사용자에게는 봇이 2초 후에 반응합니다.", message.property(Const.Message.Message).stringValue());
+        assertEquals("echoBot", message.property(Const.Message.Sender).stringValue());
+
+        final String notifyId2 = new ObjectId().toString();
+
+        rsession.tranSync(new TransactionJob<Object>() {
+            @Override
+            public Object handle(WriteSession wsession) throws Exception {
+                wsession.pathBy("/rooms/1234/messages/").removeSelf();
+                wsession.pathBy("/rooms/1234/messages/testMessage2")
+                        .property(Const.Message.Message, "Hello")
+                        .property(Const.Message.Sender, "ryun")
+                        .property(Const.Message.Event, Const.Event.onMessage);
+                wsession.pathBy("/notifies/echoBot/" + notifyId2).refTo("message", "/rooms/1234/messages/testMessage2");
+                return null;
+            }
+        });
+
+        fakeResponse = TalkResponseBuilder.create().newInner().property("notifyId", notifyId2).build();
+
+        bot.onMessage(fakeResponse);
+
+        Thread.sleep(1000);
+        message = readMessage();
+        assertEquals("Hello", message.property(Const.Message.Message).stringValue());
+        assertEquals("ryun", message.property(Const.Message.Sender).stringValue());
+
+        Thread.sleep(2000);
+        message = readMessage();
+        assertEquals("Hello", message.property(Const.Message.Message).stringValue());
+        assertEquals("echoBot", message.property(Const.Message.Sender).stringValue());
 
     }
 
     @Override
     public void tearDown() throws Exception {
-        rentry.shutdown();
-        rengine.stopForTest();
+        aradon.stop();
         super.tearDown();
+    }
+
+    public ReadNode readMessage() {
+        Iterator<String> iter = rsession.pathBy("/rooms/1234/messages/").childrenNames().iterator();
+        Debug.line(rsession.pathBy("/rooms/1234/messages/").childrenNames());
+
+        String echoMessage = null;
+        while(iter.hasNext()){
+            echoMessage = iter.next();
+            if(!StringUtil.startsWith(echoMessage, "testMessage"))
+                break;
+        }
+
+        return rsession.pathBy("/rooms/1234/messages/" + echoMessage);
     }
 }
