be8cf56fc9a8f23c74c94532dff2456656518d35
JaeHyuk, Kim <airkjh99@gmail.com>
2014-04-28 17:05:27 +0900

    International SMS now working

--- test/net/ion/message/sms/sender/SMSSenderTest.java
+++ test/net/ion/message/sms/sender/SMSSenderTest.java
@@ -5,14 +5,14 @@ import java.util.concurrent.ExecutionException;
 
 import junit.framework.TestCase;
 import net.ion.framework.util.Debug;
+import net.ion.message.sms.response.MessagingResponse;
 import net.ion.radon.aclient.NewClient;
-
-import org.apache.lucene.analysis.kr.utils.StringUtil;
 
 public class SMSSenderTest extends TestCase {
 
+	SMSSender sender = SMSSender.create(NewClient.create());
+
 	public void testCreateSender() throws IOException, ExecutionException, InterruptedException {
-		SMSSender sender = SMSSender.create(NewClient.create());
 		sender.toPhoneNo("01042216492").message("안녕").from("02", "3430", "1751").send().get();
 	}
 
@@ -27,7 +27,7 @@ public class SMSSenderTest extends TestCase {
 	}
 
 	public void testLongMessge() throws IOException {
-		SMSSender sender = SMSSender.create(NewClient.create());
+
 		PhoneMessage longContentMessage = sender.toPhoneNo("01025704848").message("가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하");
 
 		try {
@@ -35,6 +35,37 @@ public class SMSSenderTest extends TestCase {
 		} catch (IllegalArgumentException e) {
 			Debug.line(e.getMessage());
 			assertTrue(e.getMessage().indexOf("too large to send") > -1);
+		}
+	}
+
+	public void testInternationSender() throws InterruptedException, ExecutionException, IOException {
+//		String airkjhSister_USA = "+1-9176552272";
+		String jinikNum_indonesia = "+62-81295139955";
+
+		PhoneMessage internationalSMS = sender.toPhoneNo(jinikNum_indonesia).message("파이날 테스트! 이게 성공하면 더 이상은 안보냄!");
+		MessagingResponse response = internationalSMS.send().get();
+
+		assertTrue(response.getReponse().indexOf("<input type=\"hidden\" name=\"Result\" value=\"SUCCESS\">") > -1);
+	}
+	
+	public void testKoreaPhoneUsingInternational() throws InterruptedException, ExecutionException, IOException {
+		String airkjhNum = "+82-1091399660";
+		
+		PhoneMessage internationalSMS = sender.toPhoneNo(airkjhNum).message("파이날 테스트! 이게 성공하면 더 이상은 안보냄!");
+		MessagingResponse response = internationalSMS.send().get();
+
+		assertTrue(response.getReponse().indexOf("<input type=\"hidden\" name=\"Result\" value=\"SUCCESS\">") > -1);
+		
+	}
+
+	public void testInvalidInternationalNumber() throws IOException {
+		String noSplitFormat = "+11091399660";
+		
+		try {
+			sender.toPhoneNo(noSplitFormat).message("이 메세지는 가면 안됨!!").toRequest();
+		} catch (IllegalArgumentException e) {
+			Debug.line(e.getMessage());
+			assertTrue(e.getMessage().indexOf("Invalid international message format") > -1);
 		}
 	}
 
